63e3ea2303c5726875d82bfb4956b01dccb55ce1
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-02 19:47:39 +0800

    struktur dasar kode java & semua comment

---
+++ Day1.java
@@ -0,0 +1,28 @@
+
+public class Day1 {  // jadi setiap kode java itu harus ada di dalam class nah nama class nya itu Day1 harus sama dengan nama file
+    //ini adalah entry point,java akan mengeksekusi kode kita di mulai dari sini
+
+    /**
+     * @author AkhtarFauzan
+     * 
+     */
+    public static void main(String[] args) {
+        int p = 10;
+        int l = 5;
+        int hasil;
+
+        hasil = p * l;
+        System.out.println(hasil);
+
+        //single line comment  baris ini tidak akan di eksekusi
+
+        /*
+       multi line comment
+       ini tidak akan di eksekusi
+       ini tidak akan di eksekusi
+       ini tidak akan di eksekusi
+       ini tidak akan di eksekusi
+       ini tidak akan di eksekusi
+         */
+    }
+}
