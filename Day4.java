c24247d390184746e5f0ea53a714f0ccf1073be2
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-05 15:42:30 +0800

    type data numerik bilangan bulat byte,short,int,long

---
+++ Day4.java
@@ -0,0 +1,15 @@
+public class Day4 {
+    public static void main(String[] args) {
+        // Tipe bilangan bulat
+        byte umur = 18;
+        short tahunLahir = 2007;
+        int jumlahMahasiswa = 15000;
+        long jumlahPenduduk = 270000000L;
+      
+        // Menampilkan hasilnya
+        System.out.println("Umur: " + umur);
+        System.out.println("Tahun lahir: " + tahunLahir);
+        System.out.println("Jumlah mahasiswa: " + jumlahMahasiswa);
+        System.out.println("Jumlah penduduk: " + jumlahPenduduk);
+    }
+}
