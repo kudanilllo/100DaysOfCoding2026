7bb2d7986380e3ebd7634f658f1eddeba76470f4
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-06 19:43:30 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,14 @@
+
+public class Day5 {
+
+    public static void main(String[] args) {
+
+        // Tipe bilangan desimal
+        float tinggiBadan = 165.5f;
+        double luasBumi = 510072000.75;
+
+        System.out.println("Tinggi badan: " + tinggiBadan + " cm");
+        System.out.printf("Luas Bumi: %.2f km", luasBumi);
+
+    }
+}
