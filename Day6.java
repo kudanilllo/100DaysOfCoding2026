09647be58208e17c4a3fa171c448aa4293e3e1b5
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-07 19:09:10 +0800

    tipe data boolean dan char

---
+++ Day6.java
@@ -0,0 +1,9 @@
+public class Day6 {
+    public static void main(String[] args) {
+        boolean statusMahasiswa = true;  //boolean hanya dapat menyimpan satu niliai yaitu true dan false
+        char jenisKelamin = 'L'; //char hanya dapat menyimpan satu karakter 
+
+        System.out.println("Status mahasiswa: " + statusMahasiswa);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+    }
+}
