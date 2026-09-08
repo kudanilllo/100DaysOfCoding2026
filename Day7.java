890ca54189fff17a3b38a812c4b402b7fcc87828
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-08 16:15:40 +0800

    penggunaan String

---
+++ Day7.java
@@ -0,0 +1,15 @@
+public class Day7 {
+    public static void main(String[] args) {
+        String nama = "Akhtar fauzan";
+        String firstTwoNum = "20";
+        String lastTwoNum = "07";
+        String nim = "D";
+        int nimNum = 260000;
+
+        System.out.println("Nama saya: " + nama);
+        System.out.print("Saya lahir pada tahun: ");
+        System.out.println(firstTwoNum + lastTwoNum);
+        System.out.println("NIM: " + nim + nimNum);
+
+    }
+}
