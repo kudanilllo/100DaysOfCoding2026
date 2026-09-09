a5b9a670f5caaddf412e52aba4f8a82731daff4f
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-09 15:00:05 +0800

    Deklarasi,inisialisasi dan update variable

---
+++ Day8.java
@@ -0,0 +1,14 @@
+
+public class Day8 {
+
+    public static void main(String[] args) {
+        int x; //deklarasi
+
+        int umur = 18; // deklarasi int umur dan inisialisasi umur = 18
+        System.out.println("Nilai awal umur: " + umur);
+
+        umur = 19; // update variable umur = 18 > 19
+        System.out.println("Nilai umur setelah di update: " + umur);
+    }
+
+}
