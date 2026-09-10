4be15042a28d528db0c64807c3b10cbb2b2bf560
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-10 20:40:20 +0800

    konstanta

--- Day9.java
+++ Day9.java
@@ -1,8 +1,8 @@
 public class Day9 {
     public static void main(String[] args) {
-        final int UMUR = 2007;  //variable ini sudah tidak bisa di update/ubah karena konstan tidak diubah jika di ubah maka akan error
+        final int TAHUN_LAHIR = 2007;  //variable ini sudah tidak bisa di update/ubah karena konstan tidak diubah jika di ubah maka akan error
 
-            System.out.println("Umur saya: " + UMUR);
+            System.out.println("TAHUN LAHIR: " + TAHUN_LAHIR);
 
     }
 }
