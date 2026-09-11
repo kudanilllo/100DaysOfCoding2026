7ab04af3ba88ecc5bfcf9789ec9348902de8ee02
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-11 09:01:43 +0800

    Min Max value

---
+++ Day10.java
@@ -0,0 +1,13 @@
+
+public class Day10 {
+
+    public static void main(String[] args) {
+        System.out.println("Byte batas min & max value: " + Byte.MIN_VALUE + " s/d " + Byte.MAX_VALUE);
+        System.out.println("Short batas min & max value: " + Short.MIN_VALUE + " s/d " + Short.MAX_VALUE);
+        System.out.println("int batas min & max value: " + Integer.MIN_VALUE + " s/d " + Integer.MAX_VALUE);
+        System.out.println("Long batas min & max value: " + Long.MIN_VALUE + " s/d " + Long.MAX_VALUE);
+        System.out.println("Float batas min & max value: " + Float.MIN_VALUE + " s/d " + Float.MAX_VALUE);
+        System.out.println("Double batas min & max value: " + Double.MIN_VALUE + " s/d " + Double.MAX_VALUE);
+        System.out.println("Char batas min & max value: " + (int) Character.MIN_VALUE + " s/d " + (int) Character.MAX_VALUE);
+    }
+}
