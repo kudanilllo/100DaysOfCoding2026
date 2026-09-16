231d4812ec0b24342e65b71fe5ac1c88e0a9af6f
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-16 17:33:42 +0800

    operasi aritmatika *, /

---
+++ Day15.java
@@ -0,0 +1,18 @@
+
+public class Day15 {
+
+    public static void main(String[] args) {
+        //deklarasi dan inisialisasi variable
+        int a = 10;
+        int b = 5;
+        int hasil, hasil2;
+
+        //operasi
+        hasil = a * b;
+        hasil2 = a / b;
+
+        System.out.println("Hasil perkalian dari a * b adalah: " + hasil);
+        System.out.println("Hasil Pengurangan dari a / b adalah: " + hasil2);
+
+    }
+}
