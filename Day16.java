a6ae1b5c958d98af9222ec10fec4de3cab343c50
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-17 22:02:44 +0800

    modulo

---
+++ Day16.java
@@ -0,0 +1,18 @@
+public class Day16 {
+    public static void main(String[] args) {
+        // bulat positif
+        int a = 10;
+        int b = 3;
+        // bulat negatif
+        int c = -10;
+        int d = 3;
+        int hasil, hasil1;
+
+        hasil = a % b;
+        hasil1 = c % d;
+
+        System.out.println(hasil);
+        System.out.println(hasil1);
+
+    }
+}
