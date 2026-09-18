e2673ba8173c1c2745a649db9597f6bc3b044509
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-18 21:19:51 +0800

    operator assignment

---
+++ Day17.java
@@ -0,0 +1,16 @@
+public class Day17 {
+    public static void main(String[] args) {
+        int a = 20;
+
+        a += 5; // 25
+        System.out.println(a);
+        a -= 5; // 20
+        System.out.println(a);
+        a *= 2; // 40
+        System.out.println(a);
+        a /= 4; // 10
+        System.out.println(a);
+        a %= 3; // 1
+        System.out.println(a);
+    }
+}
