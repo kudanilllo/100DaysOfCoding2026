92eae22f6f19d6332936105f3c7cb31046b78e4e
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-20 18:37:02 +0800

    manual cating

---
+++ Day19.java
@@ -0,0 +1,21 @@
+public class Day19 {
+    public static void main(String[] args) {
+        double d = 9.87;
+        int i = (int) d;
+
+        System.out.println(i);
+
+        long l = 130L;
+        byte b = (byte) l;
+
+        System.out.println(b);
+
+        int angka = 65;
+        char c = (char) angka;  
+
+        System.out.println(c);
+
+
+    }
+
+}
