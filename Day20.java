decd6db61599dea5986974460bc39e0913d4509a
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-21 14:36:52 +0800

    Tipe data primitif ke String

---
+++ Day20.java
@@ -0,0 +1,21 @@
+public class Day20 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 3.14;
+        boolean c = true;
+        char d = 'A';
+        long e = 123456789L;
+
+        String s1 = String.valueOf(a); // "10"
+        String s2 = String.valueOf(b); // "3.14"
+        String s3 = String.valueOf(c); // "true"
+        String s4 = String.valueOf(d); // "A"
+        String s5 = String.valueOf(e); // "123456789"
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+    }
+}
