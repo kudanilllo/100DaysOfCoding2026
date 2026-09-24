e2d26707628dcc4e8d56e86d2079085125261a37
kudanilllo <akhtarfauzan9@gmail.com>
2026-09-24 13:00:23 +0800

    menghitung luas persegi

---
+++ Day23.java
@@ -0,0 +1,11 @@
+public class Day23 {
+    public static void main(String[] args) {
+        int sisi = 5,luas;
+
+        // proses
+        luas = sisi * sisi;
+
+        System.out.println("Luas: " + luas);
+
+    }
+}
